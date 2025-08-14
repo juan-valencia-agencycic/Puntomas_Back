232fac249484a5ebf453e37a8f8fa270f9fc4142
Diego Alejandro Bedoya Pérez <diego.bedoya@kubico.co>
2025-08-14 17:01:20 -0500

    endpoints creados

--- puntomas_prod/clases.py
+++ puntomas_prod/clases.py
@@ -6,3 +6,27 @@ class InfoClientes(BaseModel):
     nombre: str
     email: str
     edad: Optional[int] = None
+
+class ObtenerClientePorTelefonoRequest(BaseModel):
+    telefonoCli: str
+class ClienteRequest(BaseModel):
+    telefonoCli: str
+    indicador: int
+    social: str
+class PuntajeRequest(BaseModel):
+    telefonoCli: str
+    social: str
+class DisputasRequest(BaseModel):
+    telefonoCli: str
+    social: str
+class TarjetasRequest(BaseModel):
+    telefonoCli: str
+    social: str
+class PagoRequest(BaseModel):
+    telefonoCli: str
+    social: str
+class CreditInfoRequest(BaseModel):
+    telefonoCli: str
+    social: str
+class DataClienteRequest(BaseModel):
+    telefonoCli: str
